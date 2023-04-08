f899b43dd95d8b245553792027a52cf1662d003a
natalia-pavlenko <npavl56@gmail.com>
2023-04-08 21:20:34 +0300

    comm2

--- src/page/Register.js
+++ src/page/Register.js
@@ -1,21 +1,21 @@
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
 
-export default function RegisterView() {
-const dispatch = useDispatch ();
-const [name, setName] = useState ('');
-const [email, setEmail] = useState ('');
-const [password, setPassword] = useState('');
+// export default function RegisterView() {
+// const dispatch = useDispatch ();
+// const [name, setName] = useState ('');
+// const [email, setEmail] = useState ('');
+// const [password, setPassword] = useState('');
 
-}
+// }
 
-const hendleSubmit = e => {
-e.preventDefault();
-dispatch(authOperations.register({name, email, password}));
-setName('');
-setEmail('');
-setPassword();
-}
+// const hendleSubmit = e => {
+// e.preventDefault();
+// dispatch(authOperations.register({name, email, password}));
+// setName('');
+// setEmail('');
+// setPassword();
+// }
 
-return <div>
-    <h1> страница регистрации</h1>
-</div>
+// return <div>
+//     <h1> страница регистрации</h1>
+// </div>
